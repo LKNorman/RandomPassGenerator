da235814446c5dcb7c0ec746e1cc2b1735093d4e
LKNorman <lknorman30@gmail.com>
2020-03-06 20:27:56 -0500

    added variables for pass

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ while (isNaN(length) || length < 8 || length > 128)
       "Length must be 8-128 characters. How many characters would you like your password to be?"
     )
   );
+  
+// confirms for the different character types
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
